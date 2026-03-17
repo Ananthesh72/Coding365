984ddf8f91f80db2c8ddb6a18305259a868cbaca
Ananthesh72 <anantheshshenoy72@gmail.com>
2026-03-17 23:42:41 +0530

    Added Geeks for Geeks minmax2nd num

---
+++ Geeks for Geeks/TCS NQT problems/minAnd2ndMin.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class minAnd2ndMin {
+    public static void main(String[] args) {
+
+        int first = Integer.MAX_VALUE;
+        int second = Integer.MAX_VALUE;
+
+        for (int i : arr) {
+            if (i < first) {
+                second = first;
+                first = i;
+            } else if (i < second && i != first) {
+                second = i;
+            }
+        }
+
+        ArrayList<Integer> result = new ArrayList<>();
+        // If second stayed as MAX_VALUE, it means no second distinct min exists
+        if (second == Integer.MAX_VALUE) {
+            result.add(-1);
+        } else {
+            result.add(first);
+            result.add(second);
+        }
+        return result;
+
+    }
+}
