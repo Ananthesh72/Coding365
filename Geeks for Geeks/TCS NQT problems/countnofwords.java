379ad0a976f5ac7d10216832fd3a2e6564c4e944
Ananthesh72 <anantheshshenoy72@gmail.com>
2026-03-22 20:09:54 +0530

    Added Geeks for Geeks Added countnofwords

---
+++ Geeks for Geeks/TCS NQT problems/countnofwords.java
@@ -0,0 +1,14 @@
+public class countnofwords {
+    public static void main(String[] args) {
+        String s ="a\nhjpfo";
+        // String s ="a\nyo\n";
+
+
+        String cleaned = s.replace("\\n", " ").replace("\\t", " ");
+        String trimmed = cleaned.trim();
+        if (trimmed.isEmpty()) {
+            System.out.println(0);;
+        }
+        System.out.println(trimmed.split("\\s+").length);
+    }
+}
