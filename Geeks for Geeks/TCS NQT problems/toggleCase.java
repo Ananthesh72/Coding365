8e53a76893f71d1541395a839095da1e478a6f80
Ananthesh72 <anantheshshenoy72@gmail.com>
2026-03-22 22:16:43 +0530

    Added Geeks for Geeks Added EncryptString

---
+++ Geeks for Geeks/TCS NQT problems/toggleCase.java
@@ -0,0 +1,17 @@
+public class toggleCase {
+    public static void main(String[] args) {
+        String s = "geeksForgEeks";//GEEKSfORGeEKS
+        int n = s.length();
+     
+        StringBuilder sb = new StringBuilder();
+        for(Character ch : s.toCharArray()){
+            
+            if(Character.isUpperCase(ch)){
+                sb.append(Character.toLowerCase(ch));
+            }else{
+                sb.append(Character.toUpperCase(ch));
+            }
+        }
+        System.out.println(sb);
+    }
+}
